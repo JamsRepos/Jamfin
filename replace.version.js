b67141d69cb990531d2b6884ff8bb21f6545c5b0
Jam <1347620+JamsRepos@users.noreply.github.com>
2024-06-02 23:02:03 +0100

    refactor: :rocket: Ensure files are added to git prior to commit

--- replace.version.js
+++ replace.version.js
@@ -1,15 +1,22 @@
 const fs = require('fs');
 const path = require('path');
+const { execSync } = require('child_process');
 
 const version = process.argv[2];
 const directory = process.argv[3];
 
 const replaceInFile = (filePath) => {
     const content = fs.readFileSync(filePath, 'utf8');
-    const updatedContent = content.replace(/https:\/\/cdn\.jsdelivr\.net\/gh\/JamsRepos\/Jamfin@[0-9.]*/g, `https://cdn.jsdelivr.net/gh/JamsRepos/Jamfin@${version}`);
-    if (content !== updatedContent) {
+    const regex = /https:\/\/cdn\.jsdelivr\.net\/gh\/JamsRepos\/Jamfin@[0-9.]*/g;
+    const matches = content.match(regex);
+
+    if (matches) {
+        const updatedContent = content.replace(regex, `https://cdn.jsdelivr.net/gh/JamsRepos/Jamfin@${version}`);
         fs.writeFileSync(filePath, updatedContent, 'utf8');
-        console.log(`Updated file: ${filePath}`);
+        console.log(`Updated file: ${filePath} with new version: ${version}`);
+
+        // Stage the updated file for commit
+        execSync(`git add ${filePath}`);
     }
 };
 
